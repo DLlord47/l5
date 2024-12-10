10ba73b5565db1e83c85b1f74d5d6d2e4c222271
Бычков Алексей Андреевич <a.bychkov@univ.priv>
2024-12-10 10:18:32 +0300

    added useless function

--- n1.py
+++ n1.py
@@ -1,16 +1,21 @@
 from random import randint
+
+
+def useless_function(*args):
+    return args
 def turn(c, ch):
     print(f"Ход: {ch}.")
     return c - ch
 
-n = randint(4,30)
+
+n = randint(4, 30)
 t = True
 while n > 1:
     print(f"В куче {n} камней.")
     if t:
         n = turn(n, int(input('Сделай ход: ')))
     else:
-        n = turn(n, randint(1,3))
+        n = turn(n, randint(1, 3))
     t = not t
 if n == 1:
     print('Победа!' if not t else 'Поражение.')
